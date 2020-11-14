921b9e7e554f73480dae1a6d4d3f6521309933c9
exhlim <exhlimm@gmail.com>
2020-11-15 02:37:29 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-32ef35d4b8c70691601a.js"
+    "url": "webpack-runtime-28d79bed6f3f5034e9e4.js"
   },
   {
     "url": "framework-2aff3dd42eb60cb6c774.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d8da3614dc308214cd49a2a34cc82019"
+    "revision": "57f8a2b29984edd3175867dfe56814ab"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "092f9524224befc43884f0aa22b7aa76"
+    "revision": "1cd24a1468f3f3c06676ad37f299e338"
   },
   {
     "url": "manifest.json",
